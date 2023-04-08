1b92da4c88382a1361fb84e2c314a0194e7098d0
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-04-08 23:08:38 +0530

    initial setup + user, booking_type and booking models relatonships

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :booking_types
+  resources :bookings
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
 
 
   devise_for :users
+
+  authenticated :user do
+    root to: "home#dashboard", as: :authenticated_root
+  end
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
